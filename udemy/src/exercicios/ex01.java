913507edd0dbe8343772eddaaa8f9f040e3916ef
arthur <arthur@DESKTOP-CIIUOUC>
2023-05-16 23:17:54 -0300

    atualização de documentos em outra pasta

---
+++ udemy/src/exercicios/ex01.java
@@ -0,0 +1,49 @@
+package exercicios;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+
+		// Segmento A
+
+		int q = 5;
+		switch( q )
+		{
+		   case 1:
+		     System.out.println( 1 );
+		   case 2:
+		     System.out.println( 2 );
+		   case 3:
+		     System.out.println( 3 );
+		   case 4:
+		     System.out.println( 4 );
+		   case 5:
+		     System.out.println( 5 );
+		   default:
+		     System.out.println( "default" );
+		} // fim do switch
+
+//		// Segmento B
+//		int q = 5;
+//		switch( q )
+//		{
+//		   case 1:
+//		     System.out.println( 1 );
+//		   case 2:
+//		     System.out.println( 2 );
+//		   case 3:
+//		     System.out.println( 3 );
+//		   case 4:
+//		     System.out.println( 4 );
+//		   case 5:
+//		     System.out.println( 5 );
+//		   default:
+//		     System.out.println( "default" );
+//		} // fim do switch
+		
+		   } // fim de main
+		} // fim da classe TestB
+		
+		
+	
+
